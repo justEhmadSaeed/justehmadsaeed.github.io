6eb9bf7586360b8cf337849059c4fc00fa212626
Ehmad Saeed <justehmadsaeed@gmail.com>
2023-04-10 23:21:07 +0500

    feat: update content and optimize the assets

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,29 @@ var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
 
 var Paths = {
-  HERE: './',
-  DIST: 'dist/',
-  CSS: './assets/css/',
-  SCSS_TOOLKIT_SOURCES: './assets/scss/blk-design-system.scss',
-  SCSS: './assets/scss/**/**'
+	HERE: './',
+	DIST: 'dist/',
+	CSS: './assets/css/',
+	SCSS_TOOLKIT_SOURCES: './assets/scss/blk-design-system.scss',
+	SCSS: './assets/scss/**/**',
 };
 
-gulp.task('compile-scss', function() {
-  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write(Paths.HERE))
-    .pipe(gulp.dest(Paths.CSS));
+gulp.task('compile-scss', function () {
+	return gulp
+		.src(Paths.SCSS_TOOLKIT_SOURCES)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(sourcemaps.write(Paths.HERE))
+		.pipe(gulp.dest(Paths.CSS));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(Paths.SCSS, ['compile-scss']);
+gulp.task('watch', function () {
+	gulp.watch(Paths.SCSS, ['compile-scss']);
 });
 
-gulp.task('open', function() {
-  gulp.src('index.html')
-    .pipe(open());
+gulp.task('open', function () {
+	gulp.src('index.html').pipe(open());
 });
 
-gulp.task('open-app', ['open', 'watch']);
+gulp.task('open-app', gulp.series('open', 'watch'));
